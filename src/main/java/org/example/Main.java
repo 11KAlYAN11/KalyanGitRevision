f0e480c5c959a8e243c057cb777b59575065935c
11kalyan11 <programmer143143@gmail.com>
2024-05-05 18:18:11 +0530

    added smtg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-            System.out.println("There is a way to create a Merge conflicts..g");
+            System.out.println("merge conflit added smtg..!");
         }
     }
 }
