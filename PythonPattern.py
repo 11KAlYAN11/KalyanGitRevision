25bd6397bce7a7f6f2829268d087c24b360eb884
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-05-07 16:45:35 +0000

    branch is not there if we try to push to it

--- PythonPattern.py
+++ PythonPattern.py
@@ -3,3 +3,4 @@ for i in range(1,n):
     print(i*"*")
 
 print("Actually branch is deleted let's what will happen if we push.")    
+print("Let's see if branch is not there but if we try to push to xyz. then shall git will create a repositary fo us? let's see..")
