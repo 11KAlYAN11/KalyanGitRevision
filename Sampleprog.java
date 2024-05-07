b4d939b8c5280e6f7b40e35ac5d9165f0fa68e89
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-05-07 16:55:36 +0000

    files added

---
+++ Sampleprog.java
@@ -0,0 +1,9 @@
+public class Sampleprog {
+    public static void main(String[] args) {
+        System.out.println("Hello which also working fine as IntellijIdea..");
+    }
+    public static fun() {
+            System.out.println("Hello this is a sample function.");
+    }
+    
+}
