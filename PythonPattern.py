0c8a4123d77cc16323d11c5c21cce9c0b4999f66
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 22:27:15 +0530

    Update PythonPattern.py

--- PythonPattern.py
+++ PythonPattern.py
@@ -1,6 +1,7 @@
 n = int(input("Enter num: "))
 for i in range(1,n):
     print(i*"*")
+print("It's done!")    
 
 print("Actually branch is deleted let's what will happen if we push.")    
 print("Let's see if branch is not there but if we try to push to xyz. then shall git will create a repositary fo us? let's see..")
